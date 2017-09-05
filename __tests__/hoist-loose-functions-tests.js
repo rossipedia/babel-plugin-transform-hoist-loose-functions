0233aca4c40437dc5595fc7fc541b3899364f340
Bryan Ross <git@rossipedia.com>
2017-09-05 13:16:08 -0600

    initial commit

---
+++ __tests__/hoist-loose-functions-tests.js
@@ -0,0 +1,288 @@
+jest.autoMockOff();
+
+const thePlugin = require("../utils/test-transform")(require("../src/index"));
+
+describe("hoist-loose-functions-plugin", () => {
+  describe("loose mode", () => {
+    thePlugin(
+      "should hoist functions in top-level if blocks",
+      `
+      if (a) {
+        function bar() {}
+      }
+    `,
+      `
+      function bar() {}
+      if (a) {}
+    `
+    );
+
+    thePlugin(
+      "should hoist nested functions in if blocks",
+      `
+      function foo(a) {
+        if (a) {
+          function bar() {}
+        }
+      }
+    `,
+      `
+      function foo(a) {
+        function bar() {}
+
+        if (a) {}
+      }
+    `
+    );
+
+    thePlugin(
+      "should hoist functions in top-level while blocks",
+      `
+      while (true) {
+        function foo() {}
+      }
+    `,
+      `
+      function foo() {}
+      while (true) {}
+    `
+    );
+
+    thePlugin(
+      "should hoist nested functions in while blocks",
+      `
+      function foo() {
+        while (true) {
+          function bar() {}
+        }
+      }
+    `,
+      `
+      function foo() {
+        function bar() {}
+
+        while (true) {}
+      }
+    `
+    );
+
+    thePlugin(
+      "should hoist functions in top-level do blocks",
+      `
+      do {
+        function foo() {}
+      } while (true);
+    `,
+      `
+      function foo() {}
+      do {} while (true);
+    `
+    );
+
+    thePlugin(
+      "should hoist nested functions in do blocks",
+      `
+      function foo() {
+        do {
+          function bar() {}
+        } while (true);
+      }
+    `,
+      `
+      function foo() {
+        function bar() {}
+
+        do {} while (true);
+      }
+    `
+    );
+
+    thePlugin(
+      "should hoist functions in top-level anonymous blocks",
+      `
+      var a = 0;
+      {
+        function foo() {}
+      }
+      var b = 1;
+    `,
+      `
+      function foo() {}
+      var a = 0;
+      {}
+      var b = 1;
+    `
+    );
+
+    thePlugin(
+      "should hoist nested functions in anonymous blocks",
+      `
+      function foo() {
+        var a = 0;
+        {
+          function bar() {}
+        }
+        var b = 1;
+      }
+    `,
+      `
+      function foo() {
+        function bar() {}
+
+        var a = 0;
+        {}
+        var b = 1;
+      }
+    `
+    );
+
+    thePlugin(
+      "should only hoist to nearest function",
+      `
+      function outer() {
+        function inner() {
+          if (a) {
+            function foo() {}
+          }
+        }
+      }
+    `,
+      `
+      function outer() {
+        function inner() {
+          function foo() {}
+
+          if (a) {}
+        }
+      }
+    `
+    );
+
+    thePlugin(
+      "should hoist inside function expressions",
+      `
+      var foo = function () {
+        if (true) {
+          function bar() {}
+        }
+      };
+    `,
+      `
+      var foo = function () {
+        function bar() {}
+
+        if (true) {}
+      };
+    `
+    );
+  });
+
+  describe("strict mode", () => {
+    thePlugin(
+      "should not hoist top-level if program is strict",
+      `
+      'use strict';
+
+      if (true) {
+        function foo() {}
+      }
+      `
+    );
+
+    thePlugin(
+      "should not hoist nested if program is strict",
+      `
+      'use strict';
+
+      function foo() {
+        if (true) {
+          function bar() {}
+        }
+      }
+      `
+    );
+
+    thePlugin(
+      "should not hoist nested if outer function is strict",
+      `
+      'use strict';
+
+      function foo() {
+        'use strict';
+
+        if (true) {
+          function bar() {}
+        }
+      }
+      `
+    );
+
+    thePlugin(
+      "should hoist strict function if outer function is loose",
+      `
+      function foo() {
+        if (true) {
+          function bar () {
+            'use strict';
+          }
+        }
+      }
+    `,
+      `
+      function foo() {
+        function bar() {
+          'use strict';
+        }
+
+        if (true) {}
+      }
+    `
+    );
+  });
+
+  describe("methods", () => {
+    thePlugin(
+      "should hoist functions in class methods",
+      `
+      class Foo {
+        bar() {
+          if (true) {
+            function foo() {}
+          }
+        }
+      }
+    `,
+      `
+      class Foo {
+        bar() {
+          function foo() {}
+
+          if (true) {}
+        }
+      }
+    `
+    );
+
+    thePlugin(
+      "should hoist functions in object methods",
+      `
+      const obj = {
+        bar() {
+          if (true) {
+            function foo() {}
+          }
+        }
+      };
+    `,
+      `
+      const obj = {
+        bar() {
+          function foo() {}
+
+          if (true) {}
+        }
+      };
+    `
+    );
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function hasStrictModeDirective(path) {
+  const { directives } = path.isProgram() ? path.node : path.node.body;
+  return directives.some(
+    d =>
+      d.value &&
+      d.value.type === "DirectiveLiteral" &&
+      d.value.value === "use strict"
+  );
+}
+
+function isStrictMode(path) {
+  while (path) {
+    if (hasStrictModeDirective(path)) {
+      return true;
+    }
+    path = path.getFunctionParent();
+  }
+  return false;
+}
+
+function isHoisted(path, parentFn) {
+  const parentScope = path.scope.parent;
+  const fnScope = parentFn.scope;
+
+  return path.scope.parent === parentFn.scope;
+}
+
+module.exports = function({ types: t }) {
+  return {
+    name: "transform-hoist-loose-functions",
+    visitor: {
+      Program(path) {
+        if (hasStrictModeDirective(path)) {
+          path.stop();
+        }
+      },
+
+      FunctionDeclaration: {
+        exit(path) {
+          const parentFn = path.getFunctionParent();
+          if (isStrictMode(parentFn) || isHoisted(path, parentFn)) {
+            return;
+          }
+
+          const container = parentFn.isProgram()
+            ? parentFn
+            : parentFn.get("body");
+          container.unshiftContainer("body", t.clone(path.node))[0].skip();
+          path.remove();
+        }
+      }
+    }
+  };
+};
